e13dc271d34ca058a1c40607c4fee1d65aac6d16
notfound404 <notfound@heenam.co.kr>
2019-08-01 16:05:46 +0900

    샌드박스 테스트 주석 변경.

--- codefTest/src/main/java/com/codef/io/sandbox/SandboxApiRequest.java
+++ codefTest/src/main/java/com/codef/io/sandbox/SandboxApiRequest.java
@@ -25,6 +25,7 @@ public class SandboxApiRequest {
 		// 리소스서버 접근을 위한 액세스토큰 설정(기존에 발급 받은 토큰이 있다면 유효기간 만료까지 재사용)
 		String accessToken = CommonConstant.ACCESS_TOKEN;								
 		if(StringUtils.isEmpty(accessToken)) {	// 액세스토큰을 발급받은 적이 없다면 토큰 발급 요청 수행
+			System.out.println("====    액세스토큰을 발급받은 적이 없다면 토큰 발급 요청 수행");
 			accessToken = RequestToken.getToken(CommonConstant.SANDBOX_CLIENT_ID, CommonConstant.SANDBOX_SECERET_KEY);
 			CommonConstant.ACCESS_TOKEN = accessToken;	// 재사용을 위한 발급받은 액세스 토큰 저장
 		}
@@ -34,17 +35,22 @@ public class SandboxApiRequest {
 		bodyString = URLEncoder.encode(bodyString, "UTF-8");
 		
 		// API 요청
+		System.out.println("====    API 요청");
 		JSONObject json = (JSONObject)HttpRequest.post(urlPath, accessToken, bodyString);
+		System.out.println("====    API 요청 결과 확인");
 		String result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
 		
 		// 액세스 토큰 유효기간 만료시
 		if("invalid_token".equals(json.get("error"))) {
+			System.out.println("====    유효하지 않은 토큰인 경우 토큰 재발급 요청");
 			// 토큰 재발급 요청 수행
 			accessToken = RequestToken.getToken(CommonConstant.SANDBOX_CLIENT_ID, CommonConstant.SANDBOX_SECERET_KEY);
 			CommonConstant.ACCESS_TOKEN = accessToken;	// 재사용을 위한 발급받은 액세스 토큰 저장
 		
 			// API 재요청
+			System.out.println("====    API 요청");
 			json = (JSONObject)HttpRequest.post(urlPath, accessToken, bodyString);
+			System.out.println("====    API 요청 결과 확인");
 			result = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(json);
 			
 		} else if("access_denied".equals(json.get("error"))) {
