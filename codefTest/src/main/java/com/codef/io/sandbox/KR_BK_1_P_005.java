4b84696fa71b95c0571318292a5168c642f37356
notfound404 <notfound@heenam.co.kr>
2019-08-07 10:08:35 +0900

    빠른 계좌 조회 테스트 코드 반영.

--- codefTest/src/main/java/com/codef/io/sandbox/KR_BK_1_P_005.java
+++ codefTest/src/main/java/com/codef/io/sandbox/KR_BK_1_P_005.java
@@ -1,33 +1,41 @@
 package com.codef.io.sandbox;
 
 import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.HashMap;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 
 import org.json.simple.parser.ParseException;
 
 import com.codef.io.util.CommonConstant;
+import com.codef.io.util.RSAUtil;
 
 /**
- * 은행 개인 빠른계좌 거래내역		
+ * 은행 개인 빠른계좌		
  */
 public class KR_BK_1_P_005 {
 
-	public static void main(String[] args) throws IOException, InterruptedException, ParseException {
+	public static void main(String[] args) throws IOException, InterruptedException, ParseException, InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
 		// 요청 URL 설정
 		String urlPath = CommonConstant.SANDBOX_DOMAIN + CommonConstant.KR_BK_1_P_005;
 		
-		// 요청 파라미터 설정 시작
+		// 요청 파라미터 설정 시작 - 은행사별로 요구되는 요청 파라미터가 상이합니다. 비밀번호 관련 필드가 사용되는 경우 RSA 암호화가 필요합니다.  
 		HashMap<String, Object> bodyMap = new HashMap<String, Object>();
 		
 		bodyMap.put("fastId", "");
-		bodyMap.put("fastPassword", "");
+		bodyMap.put("fastPassword", RSAUtil.encryptRSA("password", CommonConstant.PUBLIC_KEY));		// 해당 필드 사용시 RSA암호화 필요. 미사용시 공백으로 설정.	
 		bodyMap.put("id", "");
-		bodyMap.put("password", "");
+		bodyMap.put("password", RSAUtil.encryptRSA("password", CommonConstant.PUBLIC_KEY));			// 해당 필드 사용시 RSA암호화 필요. 미사용시 공백으로 설정.
 		
 		bodyMap.put("organization", "0004");
 		
 		bodyMap.put("account", "06170204160000"); 
-		bodyMap.put("accountPassword", "3500");
+		bodyMap.put("accountPassword", RSAUtil.encryptRSA("password", CommonConstant.PUBLIC_KEY));		// 해당 필드 사용시 RSA암호화 필요. 미사용시 공백으로 설정.
 		bodyMap.put("startDate", "20190301");
 		bodyMap.put("endDate", "20190619");
 		bodyMap.put("orderBy", "0");
---
+++ codefTest/src/test/java/com/codef/io/sample/bk/TestKR_BK_1_P_005.java
@@ -0,0 +1,58 @@
+package com.codef.io.sample.bk;
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.HashMap;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import org.json.simple.parser.ParseException;
+import org.junit.Test;
+
+import com.codef.io.util.ApiRequest;
+import com.codef.io.util.CommonConstant;
+import com.codef.io.util.RSAUtil;
+
+/**
+ * 은행 개인 빠른계좌 조회	
+ */
+public class TestKR_BK_1_P_005 {
+	
+	@Test
+	public void testKR_BK_1_P_005() throws IOException, InterruptedException, ParseException, InvalidKeyException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException {
+		// 요청 URL 설정
+		String urlPath = CommonConstant.getRequestDomain() + CommonConstant.KR_BK_1_P_005;
+		
+		// 요청 파라미터 설정 시작 - 은행사별로 요구되는 요청 파라미터가 상이합니다. 비밀번호 관련 필드가 사용되는 경우 RSA 암호화가 필요합니다.
+		HashMap<String, Object> bodyMap = new HashMap<String, Object>();
+		
+		bodyMap.put("id", "인터넷뱅킹 아이디");
+		bodyMap.put("password", RSAUtil.encryptRSA("인터넷뱅킹 비밀번호", CommonConstant.PUBLIC_KEY));
+		bodyMap.put("fastId", "빠른계좌조회 아이디");
+		bodyMap.put("fastPassword", RSAUtil.encryptRSA("빠른계좌조회 비밀번호", CommonConstant.PUBLIC_KEY));
+		
+		bodyMap.put("organization",	"기관코드"); 
+		
+		bodyMap.put("account",		"계좌번호");
+		bodyMap.put("accountPassword", RSAUtil.encryptRSA("계좌 비밀번호", CommonConstant.PUBLIC_KEY));
+		bodyMap.put("startDate",	"조회시작일자");
+		bodyMap.put("endDate",		"조회종료일자");
+		bodyMap.put("orderBy",		"정렬기준");
+
+		bodyMap.put("identity", "사업자번호/주민번호");
+		
+		bodyMap.put("smsAuthNo", "SMS문자인증번호");
+		
+		// 요청 파라미터 설정 종료
+		
+		// API 요청
+		String result = ApiRequest.reqeust(urlPath, bodyMap);
+
+		// 응답결과 확인
+		System.out.println(result);
+	}
+}
